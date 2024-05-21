0e4ca5fa9241db6ca70254c56a5dacde67d10e58
Sichuich <120375094+Sichuich@users.noreply.github.com>
2024-05-21 09:44:36 +0200

    final

--- src/main/java/jobshop/solvers/Solver.java
+++ src/main/java/jobshop/solvers/Solver.java
@@ -2,7 +2,6 @@ package jobshop.solvers;
 
 import jobshop.Instance;
 import jobshop.encodings.Schedule;
-import jobshop.solvers.neighborhood.Nowicki;
 
 import java.util.Optional;
 
@@ -24,12 +23,7 @@ public interface Solver {
             case "basic": return new BasicSolver();
             case "spt": return new GreedySolver(GreedySolver.Priority.SPT);
             case "lrpt": return new GreedySolver(GreedySolver.Priority.LRPT);
-            case "est_spt": return new GreedySolver(GreedySolver.Priority.EST_SPT);
-            case "est_lrpt": return new GreedySolver(GreedySolver.Priority.EST_LRPT);
-            case "descent_est_spt": return new DescentSolver(new Nowicki(), new GreedySolver(GreedySolver.Priority.EST_SPT));
-            case "descent_est_lrpt": return new DescentSolver(new Nowicki(), new GreedySolver(GreedySolver.Priority.EST_LRPT));
-            //case "taboo_est_spt": return new TabooSolver(new Nowicki(), new GreedySolver(GreedySolver.Priority.EST_SPT));
-            //case "taboo_est_lrpt": return new TabooSolver(new Nowicki(), new GreedySolver(GreedySolver.Priority.EST_LRPT));
+            // TODO: add new solvers
             default: throw new RuntimeException("Unknown solver: "+ name);
         }
     }
